be027698a57e5d26a98eb0f5e1793d35255e23b5
Naman_Punj <76200263+Naman0022@users.noreply.github.com>
2023-12-28 17:56:55 +0530

    Added Hindi Language

--- Gemini Pro/geminiVision_api.py
+++ Gemini Pro/geminiVision_api.py
@@ -1,3 +1,7 @@
+import pyautogui
+
+myScreenshot = pyautogui.screenshot()
+myScreenshot.save('Gemini Pro\\Images\\screen'+str(1)+'.png')
 import pathlib
 import textwrap
 
@@ -6,16 +10,10 @@ import google.generativeai as genai
 genai.configure(api_key='AIzaSyDL-rzwGx6VVEtln2V4n7yX8_VXAZqAZoo')
 
 model = genai.GenerativeModel('gemini-pro-vision')
+import PIL.Image
 
-img="D:\code\Python\Edith_3.1\grey-crowned-crane-bird-crane-animal.jpeg"
-while True:
-    question = input(">>")
-    if question=='q':
-        break
-    else:
-        try:
-            response = model.generate_content(["Write a short, engaging blog post based on this picture. It should include a description of the meal in the photo and talk about my journey meal prepping.", img], stream=True)
-            response.resolve()
-            print("Edith:",response.text)
-        except Exception:
-            print("Edith: Sorry I failed to generate content")
+img = PIL.Image.open('Gemini Pro\\Images\\screen.png')
+response = model.generate_content(["Can you tell me whats on my screen", img], stream=True)
+response.resolve()
+
+print(response.text)
